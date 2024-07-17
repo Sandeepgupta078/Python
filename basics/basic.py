6c5e85818fa871251037f4b1a71406be583989de
Sandeepgupta078 <sandeepmgs078@gmail.com>
2024-07-17 16:48:09 +0530

    practice basics

---
+++ basics/basic.py
@@ -0,0 +1,57 @@
+# a = 5
+# b = 7
+
+# c = a + b
+# print(c)
+
+# logical operators
+
+#  or
+# print("true or false is", True or False)
+# print("true or true is", True or True)
+# print("false or true is", False or True)
+# print("false or false is", False or False)
+
+
+#  and
+# print("true  and false is", True and False)
+# print("true and true is", True and True)
+# print("false and true is", False and True)
+# print("false and false is", False and False)
+
+# type casting
+# a = 5
+# t = type(a)
+# print(t)
+
+# a = "5"
+# b = int(a)
+# t = type(b)
+# print(t)
+
+
+# # input
+# a = int(input("Enter no. 1: "))
+# b = int(input("Enter no. 2: "))
+
+# print("No. a is", a)
+# print("No. b is", b)
+# print("Sum is", a + b)
+
+# average of two no.
+
+# a = int(input("Enter the first no.: "))
+# b = int(input("Enter the second no.: "))
+
+# avg = (a + b) / 2
+
+# print("Average is", avg)
+
+
+# String
+
+name = 'Sandeep'
+length = len(name)
+print(length)
+print(name[0:]) # same as print(name[0:7])
+print(name[0:3])
